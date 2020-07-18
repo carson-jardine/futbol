666257729c6bf1176927924a197e3a9e71f6655a
Brett Turing <bjsherman80@gmail.com>
2020-07-18 13:14:06 -0600

    Commit to merge

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,16 +1,16 @@
-
+require_relative './helper_class'
 
 class StatTracker
 
-  def self.from_csv(locations)
-    StatTracker.new(locations)
+  def self.from_csv(filepath)
+    StatTracker.new(filepath)
   end
 
 
-  def initialize(locations)
-    @locations = locations
+  def initialize(filepath)
+    @filepath = filepath
 
-  end 
+  end
 
 
 end
