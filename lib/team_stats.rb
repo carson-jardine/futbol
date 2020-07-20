fbfca48056768ded906e50f14e1a8fe945e0011f
Brett Turing <bjsherman80@gmail.com>
2020-07-19 18:50:13 -0600

    Created team stats class

---
+++ lib/team_stats.rb
@@ -0,0 +1,24 @@
+require 'CSV'
+require_relative './game'
+require_relative './team'
+require_relative './game_teams'
+
+class TeamStats
+  attr_reader :teams
+  def initialize(filepath)
+    @teams = []
+    load_team(filepath)
+  end
+
+
+  def load_team(filepath)
+    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
+      @teams << Team.new(data)
+    end
+  end
+
+
+
+
+
+end
