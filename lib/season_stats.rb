a74bcaa605805187f33e0d6b301143df66e50de2
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-20 12:52:55 -0600

    First commit. Establishing branch.

---
+++ lib/season_stats.rb
@@ -0,0 +1,19 @@
+#Name of the Coach with the best win percentage for the season	String
+
+winningest_coach
+
+
+#Name of the Coach with the worst win percentage for the season	String
+worst_coach
+
+#Name of the Team with the best ratio of shots to goals for the season	String
+most_accurate_team
+
+#Name of the Team with the worst ratio of shots to goals for the season	String
+least_accurate_team
+
+#Name of the Team with the most tackles in the season	String
+most_tackles
+
+#Name of the Team with the fewest tackles in the season	String
+fewest_tackles
