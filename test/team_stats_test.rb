19f80b0b3a60f20ad31a1518c61a37336aa861bc
Brett Turing <bjsherman80@gmail.com>
2020-07-22 19:00:17 -0600

    Grouped team games by season

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -6,11 +6,11 @@ require 'pry'
 class TeamStatsTest < Minitest::Test
 
   def test_it_exists
-  team_stats = TeamStats.new("./test/fixtures/fixtures_game_teams.csv", "./test/fixtures/fixtures_games.csv", "./test/fixtures/fixtures_teams.csv")
+  team_stats = TeamStats.new("./test/brett_fixtures/fixtures_game_teams.csv", "./test/brett_fixtures/fixtures_games.csv", "./test/brett_fixtures/fixtures_teams.csv")
   end
 
   def test_team_stats_have_stats
-    team_stats = TeamStats.new("./test/fixtures/fixtures_game_teams.csv", "./test/fixtures/fixtures_games.csv", "./test/fixtures/fixtures_teams.csv")
+    team_stats = TeamStats.new("./test/brett_fixtures/fixtures_game_teams.csv", "./test/brett_fixtures/fixtures_games.csv", "./test/brett_fixtures/fixtures_teams.csv")
     assert_equal 8, team_stats.teams.count
     assert_instance_of Array, team_stats.teams
 
@@ -21,7 +21,7 @@ class TeamStatsTest < Minitest::Test
   end
 
   def test_it_can_find_team_info_by_id
-    team_stats = TeamStats.new("./test/fixtures/fixtures_game_teams.csv", "./test/fixtures/fixtures_games.csv", "./test/fixtures/fixtures_teams.csv")
+    team_stats = TeamStats.new("./test/brett_fixtures/fixtures_game_teams.csv", "./test/brett_fixtures/fixtures_games.csv", "./test/brett_fixtures/fixtures_teams.csv")
     result = team_stats.team_info(4)
 
     assert_instance_of Team, result
@@ -34,16 +34,16 @@ class TeamStatsTest < Minitest::Test
   end
 
   def test_it_can_find_season_with_highest_win_percentage
-    team_stats = TeamStats.new("./test/fixtures/fixtures_game_teams.csv", "./test/fixtures/fixtures_games.csv", "./test/fixtures/fixtures_teams.csv")
+    team_stats = TeamStats.new("./test/brett_fixtures/fixtures_game_teams.csv", "./test/brett_fixtures/fixtures_games.csv", "./test/brett_fixtures/fixtures_teams.csv")
 
-    assert_equal "Season", team_stats.best_season(6)
+    assert_equal "Season", team_stats.best_season(3)
   end
 
 
 
 
   # def test_it_returns_nil_when_no_match
-  #   team_stats = TeamStats.new("./test/fixtures/fixtures_teams.csv")
+  #   team_stats = TeamStats.new("./test/brett_fixtures/fixtures_teams.csv")
   #
   #
   #   result = merchant_collection.find_by_id(23423423)
