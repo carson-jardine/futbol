7aa31ddfdb6ad860dc2e896e19ba992db76e9ffa
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-28 16:32:53 -0600

    All Game Stats shortened

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -9,9 +9,7 @@ class GameStats
   end
 
   def load_games(filepath)
-    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
-      @games << Game.new(data)
-    end
+    CSV.foreach(filepath, headers: true, header_converters: :symbol) { |data| @games << Game.new(data) }
   end
 
   def find_by_id(game_id)
